99b416086558f46b773d2b96649a6c0053a3d783
Paweł Krawczyk <pawel.krawczyk@student.uw.edu.pl>
2014-07-19 18:38:52 +0100

    Functions added

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  getinverse <- function() inverse
+  
+  setinverse <- function() inverse <<- solve(x)
+  
+  list(get = getinverse, set = setinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if(is.null(x$get())){
+    x$set()
+  }
+  x$get()
 }
